5f393ab7283f4397cb33c27adc03baf848c96968
Martin Neitz <martin.neitz@b-coop.at>
2024-12-12 15:44:46 +0100

    Consolidation push

--- widgets.mjs
+++ widgets.mjs
@@ -10,6 +10,6 @@ export default {
     // 'thatsme-widget-broadcastgreen-subscription' : '{host}/thoregon.html#thatsme-widget-broadcastgreen-subscription',
     'upayme-checkout-session'                  : '{host}/checkout.html#easypay-application-checkout/checkoutpage/{product}',
     'upayme-checkout-widget'                   : '{host}/checkout.html#easypay-application-checkout/checkoutwidget/{product}',
-    'upayme-social-proof'                      : '{host}/upayme.html#upayme-application-conversiontools/sozialproof',
+    'upayme-social-proof'                      : '{host}/conversiontools.html#upayme-application-conversiontools/sozialproof',
 
 }
